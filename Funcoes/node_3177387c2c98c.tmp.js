454be0e54e1eb103f36805787ff0e6826d490afd
Ivan Meirelles <hackermeirelles+github@gmail.com>
2023-04-29 12:07:50 -0300

    Up

---
+++ Funcoes/node_3177387c2c98c.tmp.js
@@ -0,0 +1,16 @@
+//Função: Trecho de code que só executa quando e chamamdo/Invocado
+
+//Função void(vazia)
+//Função com Parâmetro(s)
+//Função Return
+//Função Arrow / Arrow Function
+
+//Função Vazia
+
+let valor = 20
+
+function incrementa() {
+    valor = valor + 30
+}
+
+console.log(`O valor atual é: ${valor}`)
